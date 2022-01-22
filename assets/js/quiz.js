b61032724e3e891ac41fb447bee794614929de44
ngranzow <nate.granzow@gmail.com>
2022-01-22 16:47:24 -0600

    created questions

---
+++ assets/js/quiz.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        question: "Which of the following is used for assigning a value to a variable?",
+        answers: [
+            { text: "x", correct: false },
+            { text: "=", correct: true },
+            { text: "-", correct: false },
+            { text: ".", correct: false}
+        ]
+    },
+    {
+        question: "What is the correct way to write an array?",
+        answers: [
+            { text: "let animals = ('dog', 'cat', 'mouse)", correct: false },
+            { text: "let animals = {'dog', 'cat', 'mouse'}", correct: false },
+            { text: "let animals = ['dog', 'cat', 'mouse']", correct: true },
+            { text: "let animals = 'dog', 'cat', 'mouse'", correct: false }
+        ]
+    },
+    {
+        question: "Commonly used data types DO NOT inlcude:",
+        answers: [
+            { text: "strings", correct: false },
+            { text: "null", correct: false },
+            { text: "alerts", correct: true },
+            { text: "booleans", correct: false}
+        ]
+    },
+    {
+        question: 'Where does this go <script src="scripts/main.js"></script> in your HTML?',
+        answers: [
+            { text: "Just before the closing </body> tag", correct: true },
+            { text: "Inside the <head></head> tag", correct: false },
+            { text: "Just before the closing </html> tag", correct: false },
+            { text: "At the beginning of the <body> tag", correct: false}
+        ]
+    }
+]
+
